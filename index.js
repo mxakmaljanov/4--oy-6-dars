e16495f0708c97058366d80cb758e87dd70b4438
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-27 18:57:59 +0500

    4-oy 6-dars

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// 1-misol
+// let arr = [];
+// let num = +prompt("son kiriting");
+// for(let i = 1; i <= num; i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+// 2-misol
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let juft = 0;
+// let toq = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 ==1) {
+//         toq += arr[i];
+//     } else{
+//         juft += arr[i];
+//     }
+// }
+// console.log("toq sonlar yeg'indisi", toq);
+// console.log("juft sonlar yeg'indisi", juft);
+// 3-misol
+// let sum = 0;
+// let arr1 = [1,2,3,4];
+// let arr2 = [5,6,7,8];
+// let res = arr1.concat(arr2);
+// for(let i = 0; i < res.length; i++){
+//     sum += res[i];
+// }
+// console.log(sum);
+// 4-misol
+// let arr = [1,2,3,4,5,6];
+// let res = [];
+// for(let i = 0; i < arr.length; i++){
+//     res.unshift(arr[i]);
+// }
+// console.log(res);
+// 5-misol
+// let arr = [3,5,7,9];
+// let min = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (min > arr[i]) {
+//       min = arr[i]
+//     }
+//   }
+//   let res = min * min * min * min;
+//   console.log(res);
 //2-misol
 // let arr = ["hello world", 1,2,3];
 // let res ;
