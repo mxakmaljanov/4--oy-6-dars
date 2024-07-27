94aecd5c9167ad0526070462628b3da7a0f4ee61
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-27 18:28:01 +0500

    4-oy 6-dars

---
+++ index.js
@@ -0,0 +1,88 @@
+//2-misol
+// let arr = ["hello world", 1,2,3];
+// let res ;
+// for(let i = 0; i<arr.length; i++){
+// if (typeof arr[i] == 'string') {
+//     res = arr[i].length
+// }
+// }
+// console.log(res);
+// 3-misol
+// let arr = [1,2,3,4,5];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     res.push((arr[i] += 1));
+//   }
+// }
+// console.log(res);
+// 4-misol
+// let arr = [2,3,3,8,22,12];
+// for(let i = arr.length-1; i>=0; i--){
+// if (arr[i] % 2 == 1) {
+//     arr[i] = 0
+// }
+// }
+// console.log(arr)
+// 5-misol
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = i;
+// }
+// console.log(arr);
+// 6-misol
+// let arr = [4,15,20,30,60];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 != 0) {
+//         res.push(arr[i])
+//     }
+// }
+// console.log(res);
+// 7-misol
+// let arr = [1,2,3,4,5,6];
+// let res = [];
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         sum = sum + arr[i];
+//     }
+//     if(sum % arr[i] == 0){
+//         res.push(arr[i])
+//     }
+// }
+// console.log(sum);
+// console.log(res);
+// 8-misol
+// let arr = [5,6,7,8,9];
+// for (let i = 1 ; i < arr.length; i++) {
+//     arr[i] -= 1
+// }
+// console.log(arr);
+// 9-misol
+// let arr = [1,-2,3,-4,5,-6];
+// let res = [];
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//          res.push(arr[i]);
+//     }
+// }
+// console.log(res.length);
+// 10-misol
+// let arr = [1,-2,3,-4,5,-6];
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         sum = sum + arr[i];
+//     }
+// }
+// console.log(sum);
+// 12-misol
+// let arr = [1,-2,3,-4,5,-6];
+// let p = 1;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         p = p * arr[i];
+//     }
+// }
+// console.log(p);
